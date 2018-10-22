718ad3d5d99df870711a01be258997f56a9a5ebf
JTyrell <44033281+JTyrell@users.noreply.github.com>
2018-10-22 14:57:57 -0500

    Adding the CSS. HTML and JavaScript files

---
+++ maze.js
@@ -0,0 +1,30 @@
+//var walls = document.querySelectorAll(".boundary");
+//var start = document.querySelector("#start");
+//var end = document.querySelector("#end");
+//var status = document.querySelector("#status");
+//var win = true;
+
+//  start.addEventListener("mouseover", function() {
+//    document.getElementById("status").innerHTML = "Move your mouse over the S to START!!!";
+//    for (var c = 0; c < walls.length; c++) {
+//     	walls[c].addEventListener("mouseover", function() {
+//        win = false;
+//        walls[c].style.background = "red";
+//        alert("NOPE!!! YOU LOSE! START OVER!");
+//        walls[c].style.background = "#eeeeee";
+//        document.getElementById("status").innerHTML = "HA!!! YOU LOSE!"
+//        stopPropagation();
+//      });
+//    }
+//    
+//  });
+//
+//end.addEventListener("mouseover", function() {
+//  if (win == true) {
+//    document.getElementById("status").innerHTML = "GREAT PLAYING, YOU WIN!";
+//    alert("CONGRATULATIONS! YOU WIN!");
+//  }
+//  win = true;
+//});
+//
+
